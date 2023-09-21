a1f2c8e2f4b88c3cb2ca905e8521713d6c3876e0
Martin Ronot <ronot.martin@gmail.com>
2023-09-21 09:24:05 +0200

    Identite

---
+++ src/Identite.java
@@ -0,0 +1,17 @@
+public class Identite {
+    private String nip;
+    private String nom;
+    private String prenom;
+
+    public String getNip() {
+        return nip;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+}
