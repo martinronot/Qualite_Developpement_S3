fb2945ce9e0024899fed681219edea09c4985f9c
Martin Ronot <ronot.martin@gmail.com>
2023-09-26 08:36:33 +0200

    Groupe

--- src/Etudiant.java
+++ src/Etudiant.java
@@ -6,12 +6,31 @@ public class Etudiant {
     Map<String, Map<Integer, Integer>> resultats;
 
     public void addNote(String matiere, int note) {
-        if (note >= 0 && note <= 20) this.resultats[matiere][] = note;
+        if (note >= 0 && note <= 20){
+            Integer i=note;
+            this.resultats.put(matiere,i);
+        }
     }
 
     public float moyenne(String matiere) {
         int somme = 0;
         foreach ()
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public Formation getFormation(){
+        return this.formation;
     }
 
 }
