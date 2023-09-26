87041ab03a10dad000a495858b9cd584e8b1f7c2
TibereLN <lenaline2u@etu.univ-lorraine.fr>
2023-09-26 08:37:26 +0200

    Etudiant

--- src/Etudiant.java
+++ src/Etudiant.java
@@ -1,17 +1,42 @@
-import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
 
 public class Etudiant {
     Identite identite;
     Formation formation;
-    Map<String, Map<Integer, Integer>> resultats;
+    HashMap<String, List<Double>> resultats;
 
-    public void addNote(String matiere, int note) {
-        if (note >= 0 && note <= 20) this.resultats[matiere][] = note;
+    public void addNote(String matiere, double note) {
+        if (note >= 0 && note <= 20 && this.resultats.containsKey(matiere)){
+            this.resultats.get(matiere).add(0, note);
+        }
+        else{
+            System.out.println("Impossible d'ajouter une note");
+        }
     }
-
     public float moyenne(String matiere) {
-        int somme = 0;
-        foreach ()
+        int moy = 0;
+        int i = 0;
+        for (double note : this.resultats.get(matiere)) {
+            moy += note;
+            i++;
+        }
+        return moy/i;
     }
 
+    public double moyenneGenerale () {
+        double moyGenerale = 0;
+        int i = 0;
+        for (int matiere : this.resultats.keySet()) {
+            double moy = 0;
+            int j = 0;
+            for (double note : this.resultats.get(matiere)) {
+                moy += note;
+                j++;
+            }
+            moyGenerale += moy * this.formation.matieres.get(matiere);
+            i++;
+        }
+        return moyGenerale/i;
+    }
 }
